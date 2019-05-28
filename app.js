2ce30a5fcaf0ef44cb84a81e165812f287db9b34
Victorypa <victorypa1992@yandex.ru>
2019-05-28 14:24:28 +0300

    delete book from list

--- app.js
+++ app.js
@@ -44,10 +44,14 @@ UI.prototype.showAlert = function (message, className) {
     setTimeout(function () {
         document.querySelector('.alert').remove();
     }, 3000);
-
-    console.log(div)
-
 }
+
+// Delete Book
+UI.prototype.deleteBook = function(target){
+    if(target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
 
 // Clear Fields
 UI.prototype.clearFields = function () {
@@ -56,7 +60,7 @@ UI.prototype.clearFields = function () {
     document.getElementById('isbn').value = '';
 }
 
-//   Event Listeners
+//   Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
 
     const title = document.getElementById('title').value,
@@ -90,3 +94,17 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
 
     e.preventDefault();
 });
+
+//   Event Listener for delete book
+document.getElementById('book-list').addEventListener('click',
+    function (e) {
+        // Instantiate UI
+        const ui = new UI();
+
+        // Delete book
+        ui.deleteBook(e.target);
+
+        // Show message
+        ui.showAlert('Book removed', 'success');
+        e.preventDefault();
+    });
